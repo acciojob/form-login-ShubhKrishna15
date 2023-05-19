128f97a6e070d67163733cb026ccc47c1bf9e491
Shubh Krishna Shrivastava <118735729+ShubhKrishna15@users.noreply.github.com>
2023-05-19 16:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	var msg=document.getElementById("input1").value;
+	var ne=document.getElementById("input2").value;
+    alert( msg + " " +ne);
 
 }
